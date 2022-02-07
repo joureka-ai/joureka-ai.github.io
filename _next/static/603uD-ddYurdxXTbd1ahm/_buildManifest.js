5a3c48d377ff17dd4ddb4711bda0e629aa16b50b
anatomi <anatomi@users.noreply.github.com>
2022-02-07 07:40:31 +0000

    deploy: 61d43b69d056052f9f87f7f82c928b49fb44ccc5

---
+++ _next/static/603uD-ddYurdxXTbd1ahm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/917-029b5a8e13d6d4b4.js","static/css/995da0f8ca59e74c.css","static/chunks/pages/index-7c418736cc416b76.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
